922b097de0b95b2f79c6718ed562e065719ead15
GoktugGok <gokgoktug0@gmail.com>
2023-08-16 17:45:18 +0300

    upload_post

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
+    path('settings', views.settings, name="settings"),
+    path('upload', views.upload, name="upload"),
     path('signup',views.signup,name="signup"),
     path('signin',views.signin,name="signin"),
     path('logout',views.Logout,name="logout"),
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,14 +1,58 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User 
 from django.contrib.auth import authenticate , login, logout
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # Create your views here.
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user_profile = Profile.objects.get(user=request.user)
+    return render(request,'index.html',{'user_profile':user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+        return redirect('index')
+    else:
+        return redirect('index')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+    content = { 
+        'user_profile': user_profile
+    }
+    return render(request,'settings.html',content)
 
 def signup(request):
     if request.method == 'POST':
@@ -52,7 +96,8 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request,'signin.html')
-    
+
+@login_required(login_url='signin')
 def Logout(request):
     logout(request)
     return redirect('signin')
